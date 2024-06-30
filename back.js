bebd890d2820288cf316d1ffaafe2d84ea0e2024
Samarthsvs <SADGURUKRUPAAUTOSERVICES@GMAIL.COM>
2024-06-30 20:29:50 +0530

    Delete back.js

--- back.js
+++
@@ -1,8 +0,0 @@
-function generateotp(length){
-    let otp="";
-    for (let i=0; i < length; i++) {
-        otp += math.floor(math.random()*10).tostring() ;
-      }
-      return otp;
-}
-console.log(generateotp(4));
